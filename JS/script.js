a2e94246c656c8d25cf4cdbc361664c9541bc881
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2020-11-07 16:52:28 -0500

    Push JS Function

--- JS/script.js
+++ JS/script.js
@@ -10,5 +10,23 @@ function writePassword() {
 
 }
 
+// Generate Password
+
+function generatePassword (){
+
+      // Values: Numbers, Characters, Upper/Lower case.
+          var chars= "ABCDEFGHIJKLMNOPQRTSUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<>?:{}|+_)(*&^%$#@!~,./;'[]\`=-";
+          var passwordLength = 20;
+          var password="";
+
+// create for loop to choose password characters
+          for(var i=0; i < passwordLength; i++){
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber,randomNumber+1);
+          }
+
+return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
